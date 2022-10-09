e6eafd5769a03b624721895119bc3384d311cd7f
liufang <1164457816@qq.com>
2022-10-09 09:42:36 +0800

    1

--- src/views/homePage/component/center.vue
+++ src/views/homePage/component/center.vue
@@ -2,7 +2,7 @@
  * @Author: liufang 1164457816@qq.com
  * @Date: 2022-10-07 17:32:45
  * @LastEditors: liufang 1164457816@qq.com
- * @LastEditTime: 2022-10-08 17:07:29
+ * @LastEditTime: 2022-10-09 09:40:46
  * @FilePath: \relytosoft-mizar-media-uie:\project\egProject\src\views\homePage\component\center.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
--- src/views/homePage/index.vue
+++ src/views/homePage/index.vue
@@ -2,7 +2,7 @@
  * @Author: liufang 1164457816@qq.com
  * @Date: 2022-10-06 19:36:59
  * @LastEditors: liufang 1164457816@qq.com
- * @LastEditTime: 2022-10-08 17:20:49
+ * @LastEditTime: 2022-10-08 18:07:49
  * @FilePath: \relytosoft-mizar-media-uie:\project\egProject\src\views\homePage\index.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
@@ -13,26 +13,26 @@
       <CenterCom class="CenterCom"></CenterCom>
       <!-- 左右区域 -->
       <div class="content">
-        <div class="left">
+        <div   class="left animate__animated animate__bounceInLeft">
           left
-        </div>
-        <div class="right">
+        </div >
+        <div class="right animate__animated animate__bounceInRight">
           right
         </div>
       </div>
       <!-- 中部按钮 -->
-      <div class="centerBottom">
+      <div class="centerBottom animate__animated animate__bounceInUp">
 
       </div>
-<!-- 底部 -->
-<footer>
-<div>
+      <!-- 底部 -->
+      <footer>
+        <div>
 
-</div>
-<div>
-  
-</div>
-</footer>
+        </div>
+        <div>
+
+        </div>
+      </footer>
 
     </dv-full-screen-container>
 
@@ -58,7 +58,7 @@ export default {
   width: 100%;
   height: 100%;
   position: relative;
- background: radial-gradient(#444f6d, #112557, #040d2b);
+  background: radial-gradient(#444f6d, #112557, #040d2b);
 }
 #dv-full-screen-container {
   background-image: url("../../assets/images/oneborderbg.png");
@@ -71,8 +71,6 @@ export default {
   height: calc(100% - 5%);
   margin-top: 5%;
   margin: auto;
- 
-  
 }
 .content {
   display: flex;
@@ -85,6 +83,9 @@ export default {
   width: 21%;
   height: 87.7%;
   background: yellow;
+  animate-delay: 1s;  
+  animate-duration: 2s 
+  
 }
 .right {
   position: absolute;
@@ -94,36 +95,32 @@ export default {
   height: 87.7%;
   background: yellow;
 }
-.centerBottom{
+.centerBottom {
   width: 47.34%;
   height: 62px;
   background: yellow;
   position: absolute;
   bottom: 10.28%;
   left: 26.35%;
-
 }
-footer{
+footer {
   display: flex;
   justify-content: space-between;
-
 }
-footer>div:nth-child(1){
+footer > div:nth-child(1) {
   position: absolute;
   bottom: 0;
   left: 1.38%;
   width: 21%;
   height: 5%;
   background: yellow;
-
 }
-footer>div:nth-child(2){
+footer > div:nth-child(2) {
   position: absolute;
   bottom: 0;
   right: 1.38%;
   width: 21%;
   height: 5%;
   background: yellow;
-
 }
 </style>
