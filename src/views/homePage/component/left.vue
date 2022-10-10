8aca737f4712d6777c8ab19678da374d92822e36
liufang <1164457816@qq.com>
2022-10-10 21:09:08 +0800

    1

--- src/views/homePage/component/left.vue
+++ src/views/homePage/component/left.vue
@@ -2,7 +2,7 @@
  * @Author: liufang 1164457816@qq.com
  * @Date: 2022-10-09 15:38:27
  * @LastEditors: liufang 1164457816@qq.com
- * @LastEditTime: 2022-10-10 19:57:22
+ * @LastEditTime: 2022-10-10 21:09:02
  * @FilePath: \relytosoft-mizar-media-uie:\project\oficialAgency\src\views\homePage\component\left.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
@@ -146,6 +146,7 @@ section {
     font-size: 24px;
     span {
       font-size: 13px;
+      color: rgb(140, 178, 233);
     }
   }
 }
